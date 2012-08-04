86ac3faf0296ce57ffc8ea14499bbe7cecb83529
Max W Schwarz <wren@cat.pdx.edu>
2012-08-04 00:14:15 -0700

    Handle case of _help being called without args

--- Help.py
+++ Help.py
@@ -5,5 +5,11 @@ import yaml
 helpFile = open('basichelp.yaml')
 helpMessages = yaml.load(helpFile)
 
-def getHelp(topic):
-    return helpMessages[topic]
+def getHelp(topic=None):
+    if not topic:
+        topicString = "Topics: "
+        for t in helpMessages:
+            topicString += t + " "
+        return topicString
+    else:
+        return helpMessages[topic]
