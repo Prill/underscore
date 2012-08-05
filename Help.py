d191b8bcaab51809adc62764a84eea478bb4aef7
Max W Schwarz <wren@cat.pdx.edu>
2012-08-05 02:23:05 -0700

    Revamping Help command

--- Help.py
+++ Help.py
@@ -2,7 +2,8 @@
 # Basic module to load helper config
 import yaml
 
-helpFile = open('basichelp.yaml')
+#helpFile = open('basichelp.yaml')
+helpFile = open('help.yaml')
 helpMessages = yaml.load(helpFile)
 
 def getHelp(topic=None):
@@ -13,3 +14,17 @@ def getHelp(topic=None):
         return topicString
     else:
         return helpMessages[topic]
+
+def getHelp2(topic):
+    if not topic:
+        return getHelp2("about")
+    else:
+        helpBase = helpMessages[topic]
+        messages = [helpBase["topic"]]
+        if "subtopics" in helpBase and helpBase["subtopics"] != None:
+            subtopics = "Subtopics:"
+            for subt in helpBase["subtopics"]:
+                subtopics += subt + " "
+            messages.append(subtopics)
+        return messages
+
