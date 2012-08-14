30d28ce518d4d85271d9a9f6a3973755995ca133
Max W Schwarz <wren@cat.pdx.edu>
2012-08-14 03:10:24 -0700

    Slightly more helpful error messges

--- InlineTicketHandler.py
+++ InlineTicketHandler.py
@@ -5,7 +5,7 @@ from string import Template
 def inlineTicketMatch(client, user, channel, msg):
     #ticketNumbers = map(int, re.findall("#(\d{4,})", msg))
     
-    ticketNumbers = re.findall("(\w)?#(\d+)", msg)
+    ticketNumbers = re.findall("(\w+)?#(\d+)", msg)
     #print ticketNumbers
     if ticketNumbers:
         print datetime.today().strftime("%Y-%m-%d %H:%M:%S\t"), user, "requested ticket(s)", str(ticketNumbers), "in", channel
@@ -18,7 +18,7 @@ def inlineTicketMatch(client, user, channel, msg):
                     d = client.redmine_instance.getTicket(int(ticket[1]))
                     client.msg(channel, str(Template("#$id ($project) | $author | $assigned_to | $subject | $tracker").safe_substitute(d)))
                 except Exception as e:
-                    print type(e)
-                    client.msg(channel, str(e))
+                    print "%s: %s" % (ticket[1], e)
+                    client.msg(channel, "%s: %s" % (ticket[1], e))
             else:
-                client.msg(channel, "Unrecognized ticket type")
+                client.msg(channel, "Unrecognized ticket type '%s'" % ticket[0])
