8a98be7cdc5014c561c8b3be4fcabc4880cd8bf0
Max W Schwarz <wren@cat.pdx.edu>
2012-08-13 19:58:09 -0700

    Add redmine ticket parsing to InlineTicketHandler

--- InlineTicketHandler.py
+++ InlineTicketHandler.py
@@ -1,12 +1,24 @@
 import re
 import snotparser.snotparser as sp
 from datetime import datetime
-
+from string import Template
 def inlineTicketMatch(client, user, channel, msg):
-    ticketNumbers = map(int, re.findall("#(\d{4,})", msg))
-    # print ticketNumbers
-    ticketMatch = re.search("#(\d{4,})", msg)
+    #ticketNumbers = map(int, re.findall("#(\d{4,})", msg))
+    
+    ticketNumbers = re.findall("(\w)?#(\d+)", msg)
+    #print ticketNumbers
     if ticketNumbers:
         print datetime.today().strftime("%Y-%m-%d %H:%M:%S\t"), user, "requested ticket(s)", str(ticketNumbers), "in", channel
         for ticket in ticketNumbers:
-            client.msg(channel, sp.formatTicket(ticket, "$number | $from_line | $assigned_to | $subject | $flags"))
+            if ticket[0] == '':
+                if int(ticket[1]) >= 1000:
+                    client.msg(channel, sp.formatTicket(int(ticket[1]), "#$number (SNOT) | $from_line | $assigned_to | $subject | $flags"))
+            elif ticket[0] == 'c':
+                try:
+                    d = client.redmine_instance.getTicket(int(ticket[1]))
+                    client.msg(channel, str(Template("#$id ($project) | $author | $assigned_to | $subject | $tracker").safe_substitute(d)))
+                except Exception as e:
+                    print type(e)
+                    client.msg(channel, str(e))
+            else:
+                client.msg(channel, "Unrecognized ticket type")
