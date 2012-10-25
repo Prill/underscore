329f3a0c20b8b1ac83e2b38c4156a128074a4d91
Max W Schwarz <wren@cat.pdx.edu>
2012-10-25 14:33:34 -0700

    Add ticket type for testsnot

--- InlineTicketHandler.py
+++ InlineTicketHandler.py
@@ -24,11 +24,13 @@ def inlineTicketMatch(client, user, channel, msg):
     if ticketNumbers:
         print datetime.today().strftime("%Y-%m-%d %H:%M:%S\t"), user, "requested ticket(s)", formatTicketList(ticketNumbers), "in", channel
         for ticket in ticketNumbers:
-            ticket[0] = ticket[0].lower
-            if ticket[0] in ['','snot']:
+            ticketType = ticket[0].lower()
+            if ticketType in ['','snot']:
                 if int(ticket[1]) >= 1000:
                     client.msg(channel, sp.formatTicket(int(ticket[1]), "#$number (SNOT) | $from_line | $assigned_to | $subject | $flags"))
-            elif ticket[0] == 'c':
+            elif ticketType in ['testsnot']:
+                client.msg(channel, sp.formatTicket(int(ticket[1]), "#$number (TESTSNOT) | $from_line | $assigned_to | $subject | $flags", 'testsnot'))
+            elif ticketType == 'c':
                 try:
                     d = client.redmine_instance.getTicket(int(ticket[1]))
                     if d['project'] != "Mentor Sessions" or channel == "#mentors":
@@ -37,4 +39,5 @@ def inlineTicketMatch(client, user, channel, msg):
                     print "%s: %s" % (ticket[1], e)
                     client.msg(channel, "%s: %s" % (ticket[1], e))
             else:
-                client.msg(channel, "Unrecognized ticket type '%s'" % ticket[0])
+                pass
+            #    client.msg(channel, "Unrecognized ticket type '%s'" % ticket[0])
