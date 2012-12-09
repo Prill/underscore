39f1f01e7f277b8c40041277bcca32c3ea1c86ce
Max Schwarz <emesscgs@gmail.com>
2012-12-09 01:36:26 -0800

    Deleted example_config.py as obsolete

--- example_config.py
+++
@@ -1,8 +0,0 @@
-DEFAULT_SERVER = "irc.freenode.net"
-
-PREFERRED_NICK = "AzureDiamond"
-
-# Rename this file to config.py for your own installation
-NICKSERV_PASSWORD = "hunter2"
-
-DEFAULT_CHANNELS  = [("#open_channel", None), ("#private_channel", "key")] 
