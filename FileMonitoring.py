0212a6e08f850aa9093e816c7c54da8420feb880
Max Schwarz <emesscgs@gmail.com>
2012-12-23 10:40:25 -0800

    Add basic skeleton methods

--- FileMonitoring.py
+++ FileMonitoring.py
@@ -1,3 +1,10 @@
+# <FileMonitoring.py>
 # Generic interface library for monitoring files for new lines. Uses the tail
 # command and calls a user-specified method whenever a new line is added to the
 # file.
+
+def monitorFile(filepath, function):
+    pass
+
+def monitorCommand(command, args, function):
+    pass
