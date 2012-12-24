1f24219c52447ae3991bc1122b004fb1e8a41a76
Max W Schwarz <wren@cat.pdx.edu>
2012-12-23 20:39:43 -0800

    Add documentation and import statement

--- CommandMonitoring.py
+++ CommandMonitoring.py
@@ -2,8 +2,15 @@
 # Generic interface library for monitoring commands for new lines. Calls a 
 # user-specified method whenever a new line is added to the file.
 
+import subprocess
+
+# Monitors a specific file and calls function(line) for every new line that is
+# written to the end. Currently implemented as a special case of monitorCommand
+# using `tail -f` to watch the file.
 def monitorFile(filepath, function):
     pass
 
+# Monitor the output of a command and called function(line) with each new line
+# of output the command generates.
 def monitorCommand(command, args, function):
     pass
