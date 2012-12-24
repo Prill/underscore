e6f6fd30da8dc8ad38c11775be246f1d98959175
Max W Schwarz <wren@cat.pdx.edu>
2012-12-23 21:18:45 -0800

    Add and document monitorFile

--- CommandMonitoring.py
+++ CommandMonitoring.py
@@ -4,12 +4,6 @@
 
 import subprocess
 
-# Monitors a specific file and calls function(line) for every new line that is
-# written to the end. Currently implemented as a special case of monitorCommand
-# using `tail -f` to watch the file.
-def monitorFile(filepath, function):
-    pass
-
 # Monitor the output of a command and called function(line) with each new line
 # of output the command generates.
 # Input:
@@ -20,3 +14,16 @@ def monitorCommand(args, function):
     p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     for line in iter(p.stdout.readline,''):
         function(line)
+
+# Monitors a specific file and calls function(line) for every new line that is
+# written to the end. Currently implemented as a special case of monitorCommand
+# using `tail -f` to watch the file.
+# Input:
+#   filepath: Path to the file.
+#   function: Function to be called for each line. Takes one argument: the line
+#       as a string.
+#   backScroll: The number of previously written lines to iterate over when the
+#       method is first called. Defaults to 0, meaning only lines add to the
+#       file since the monitoring started will be seen.
+def monitorFile(filepath, function, backScroll=0):
+    monitorFile(["tail", "-fn", str(backScroll), filepath], function)
