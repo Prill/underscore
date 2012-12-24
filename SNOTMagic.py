03e00c588a8d00b063e6057f2bd5dd379900ff87
Max W Schwarz <wren@cat.pdx.edu>
2012-12-23 22:03:55 -0800

    Add SNOTMagic.py - Main file for magicks

---
+++ SNOTMagic.py
@@ -0,0 +1,7 @@
+# Library that monitors the SNOT logs and takes action when certain actions
+# happen.
+# For example, you can have certain people or channels be notified whenever a
+# ticket is flagged as UNIX. Or make have it alert people when a VIP0 ticket
+# arrives. Or have it tell people when somebody assigned a ticket to them. The
+# possibilities are endless
+
