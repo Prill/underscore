449c5ddedd7de35440a46269ffd7a43aad07388f
Max W Schwarz <wren@cat.pdx.edu>
2012-12-28 01:45:13 -0800

    Add logic to handle various types of messages.

--- SNOTMagic.py
+++ SNOTMagic.py
@@ -6,7 +6,8 @@
 # possibilities are endless
 
 import CommandMonitoring as cm
-import yaml
+import yaml, re
+import snotparser.snotparser as sp
 from twisted.internet import reactor
 
 # Import YAML configuration. This should really be consolidated into one
@@ -20,7 +21,35 @@ with open(CONFIG_FILE) as cfgFile:
 # each line of the snot log.
 def makeSNOTLogHandler(client):
     def handleSNOTLogLine(line):
-        client.msg(config['snot']['snot_channel'], line)
+        print line,
+        regex = r"^(?P<date>.+?) CMD: (?P<cmd>\w+?) TKT: (?P<tkt>\d+?) BY: (?P<by>.+?)($| TO: (?P<to>.+))?$"
+        match = re.match(regex, line)
+        if match:
+            mdict = match.groupdict()
+            message = str(mdict)
+            formattedTicket = sp.formatTicket(int(mdict["tkt"]), "$from_line | $assigned_to | $subject | $flags")
+            # "Case" statement for various ticket commands
+            cmd = mdict["cmd"].lower()
+            if cmd == "flags":
+                message = "#{tkt} flagged as {to} by {by}".format(**mdict)
+                if mdict['to'] in config['snot']['alerts']['flag']:
+                    for target in config['snot']['alerts']['flag'][mdict['to']]:
+                        client.msg(target, message)
+            elif cmd == "recv":
+                message = "Received ticket #{tkt} from {by}".format(**mdict)
+            elif cmd == "resp":
+                message = "{by} assigned #{tkt} to {to}".format(**mdict)
+            elif cmd == "complete":
+                message = "#{tkt} completed by {by}".format(**mdict)
+            elif cmd == "update":
+                message = "#{tkt} updated by {by}".format(**mdict)
+            elif cmd == "append":
+                message = "#{tkt} appended to #{to} by {by}".format(**mdict)
+            else:
+                message = line
+            client.msg(config['snot']['snot_channel'], message)
+        else:
+            client.msg("#snot", "Could not match '%s'" % line)
         reactor.wakeUp()
 
     return handleSNOTLogLine
